86b056764e51dc5acc1445b29d623a9b16cc4511
Mit Vasani <vasanimit9@gmail.com>
2020-10-19 14:42:49 +0530

    Create worker.js

---
+++ worker.js
@@ -0,0 +1,8 @@
+caches.open('vasanimit9.github.io.6')
+       .then(cache => {
+        cache.delete('/');
+      });
+    caches.open('vasanimit9.github.io.7')
+       .then(cache => {
+        cache.delete('/');
+    });
